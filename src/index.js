59ee7e8e4d7fce78ad4831989567d24da50de773
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2021-11-10 19:30:18 -0400

    Mock de design 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(
-  <App />,
-
-  rootElement
-)
+ReactDOM.render(<App />, document.getElementById('root'))
