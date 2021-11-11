832d9de8a50cfa77c6f4e26396d9c1b37b80516e
Your Name <bruna@fedora.(none)>
2021-11-11 19:12:29 -0300

    carrinho

--- src/App.js
+++ src/App.js
@@ -1,89 +1,90 @@
-import React from 'react';
-
-class Carrinho extends Component {
+import React from "react";
+import Carrinho from "./components/Carrinho";
  
+const produtos = [
+  { id: 1, name: "Cerveja", valor: 10 },
+  { id: 2, name: "Salada", valor: 5 },
+];
+ 
+class App extends React.Component {
   state = {
-    min: 0,
-    max: 10,
-    carrinho: [{
-      id: Date.now,
-      nome:'',
-      img: '',
-      preco:'',
-      quantidade: ''
-    }]
-
-  }
-
-  onProdutoAddCarrinho = (id) => {
-    const existe = this.state.carrinho.find(produto => id === produto.id)
-
-    if(existe) {
-      const novoCarrinho = this.state.carrinho.map(produto => {
-        if(id === produto.id) {
+    carrinho: [],
+  };
+ 
+  adicionaProduto = (id) => {
+    const produtoSelecionado = produtos.find((produto) => produto.id === id);
+ 
+    const exists = this.state.carrinho.find((elem) => elem.id === id);
+    if (exists) {
+      const novoCarrinho = this.state.carrinho.map((produto) => {
+        if (produto.id === id) {
           return {
             ...produto,
-            quantidade: produto.quantidade + 1
-          }
+            quantidade: produto.quantidade + 1,
+          };
         }
-
-        return produto
-      })
-
-      this.setState({carrinho: novoCarrinho})
+        return produto;
+      });
+      this.setState({ carrinho: novoCarrinho });
     } else {
       this.setState({
-        carrinho:[...this.state.carrinho, {nome, quantidade:1, valor}]
-      }) 
-
-  onPodutosRemovidosDoCarrinho = (id) =>
-{
-  const protudoRenovCarrinho = this.state.carrinho.map((produto) => {
-    if(produto.id === id) {
-      return {
-        ...produto,
-        quantidade: produto.quantidade - 1
-      }
+        carrinho: [
+          ...this.state.carrinho,
+          {
+            id,
+            name: produtoSelecionado.name,
+            quantidade: 1,
+            valor: produtoSelecionado.valor,
+          },
+        ],
+      });
     }
+  };
+
+  RemoveProduto = (id) => {
+    const produtoSelecionado = produtos.find((produto) => produto.id === id)
+
+    const novoCarrinho = this.state.carrinho.map((produto) => {
+      if (produto.id === id) {
+        return {
+          ...produto,
+          quantidade: produto.quantidade - 1,
+        };
+      }
     return produto
   }).filter((produto) => produto.quantidade > 0)
 
-  this.setState({carrinho: protudoRenovCarrinho})
+  this.setState({ carrinho: [
+    ...this.state.carrinho,
+    {
+      id,
+      name: produtoSelecionado.name,
+      quantidade: 1,
+      valor: produtoSelecionado.valor,
+    },
+  ],
+});
 }
 
-render() 
-  return (
-    <div style={{ borderWidth: 1, borderStyle: "solid" }}>
-      <h1>Meu carrinho de compras</h1>
-      <button
-        onClick={() =>
-          this.onProdutoAddCarrinho({ id: "1", name: "Aeronave", valor: 10 })
-        }
-      >
-        Adicionar produto 1
-      </button>
-      <button
-        onClick={() =>
-          this.adicionaProduto({ id: "2", name: "capacete-espacial", valor: 20 })
-        }
-      >
-        Adicionar produto 2
-      </button>
-      <h3>Meus itens</h3>
-      <ul>
-        {this.state.carrinho.map((item) => (
-          <li key={item.id}>
-            {`${item.nome} s- Quantidade: ${item.quantidade} - Valor: ${
-              item.preco * item.quantidade
-            }`}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )}
-}}
-
-
-
-
-export default Carrinho;
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.adicionaProduto(1)}>
+          Adiciona produto ficticio 1
+        </button>
+        <button onClick={() => this.adicionaProduto(2)}>
+          Adiciona produto ficticio 2
+        </button>
+        <button onClick={()=> this.RemoveProduto(1)}>
+        Remover
+        </button>
+        <button onClick={()=> this.RemoveProduto(2)}>
+        Remover
+        </button>
+        <Carrinho items={this.state.carrinho} />
+      </div>
+    );
+  }
+}
+ 
+export default App;
--- src/componentes/Carrinho.js
+++ src/componentes/Carrinho.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from "react";
+ 
+class Carrinho extends Component {
 
-
-CarrinhoDeCompra = () =>{
-
-    valorTotal = () => {
-        let valorTotal = 0
-    
-        for(let produto of this.produtosCarrinho) {
-          valorTotal += produto.preco * produto.quantidade
+  
+  render() {
+    return (
+      <ul>
+        { () this.props.items.map((item) => (
+          <li>{`${item.name} - Quantidade: ${item.quantidade} Valor: ${
+            item.valor * item.quantidade
+          }`}</li>
+        ))}
+      </ul>
+    );
         }
-    
-        return valorTotal
       }
-    }    
