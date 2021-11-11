2ebaabdd83ca46e3b11a7c2f9a7a12479b2973a0
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2021-11-10 20:29:03 -0400

    Mock de design 3

--- src/styles.js
+++ src/styles.js
@@ -1,34 +1,42 @@
 import styled from 'styled-components'
 
 export const BoxPagina = styled.body`
-  width: 100%;
   padding: 0;
   margin: 0;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 2fr);
+  row-gap: 10px;
+  column-gap: 10px;
 `
 export const Filtros = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  width: 15vw;
-  gap: 10px;
-  padding: 1vw;
-  border: 1px solid red;
 
+  padding: 1vw;
+  position: absolute;
+  top: 0;
+  border: 1px solid red;
+  font: 12px sans-serif;
   input {
     height: 30px;
     font-size: 15px;
+    cursor: pointer;
   }
 `
 
 export const HeaderProdutos = styled.div`
   display: flex;
-  position: absolute;
   top: 0;
-  margin-left: 18vw;
-  height: 10vh;
-  width: 60vw;
-  justify-content: center;
+
   border: 1px solid green;
+  font: 6px sans-serif;
+  justify-content: space-between;
+  select {
+    height: 5vh;
+    margin-top: 3vh;
+    cursor: pointer;
+  }
 `
 
 export const Cards = styled.div`
@@ -39,20 +47,22 @@ export const Cards = styled.div`
   width: 60vw;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(3, 1fr);
-  row-gap: 20px;
-  column-gap: 20px;
+  row-gap: 10px;
+  column-gap: 10px;
   margin-top: 20px;
   margin-left: 18vw;
 `
 
 export const CardProdutoStyle = styled.div`
   display: grid;
-
   border-style: solid;
-  border-width: thin;
+  border: 1px;
   align-items: start;
   justify-items: stretch;
   box-shadow: 0px 0px 5px 1px #868686;
+  text-align: center;
+  margin-bottom: 5px;
+  font: 12px sans-serif;
   img {
     width: 100%;
     height: 170px;
@@ -68,7 +78,7 @@ export const Carrinho = styled.div`
   position: absolute;
   top: 0;
   height: 100vh;
-  width: 17.8vw;
+  width: 17vw;
   gap: 10px;
   padding: 1vw;
   margin-left: 79vw;
